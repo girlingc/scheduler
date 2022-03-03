bbb425572c66383ff70f55874c3901aa53d07689
Christian Girling <girling.christian@gmail.com>
2022-03-03 06:01:48 -0800

    Added Cypress testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,10 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+  it("should navigate to tuesday", () => {
+    cy.visit("/");
+    cy.get("li").contains("li", "Tuesday").click()
+    .should("have.class", "day-list__item--selected");
+  });
+});
